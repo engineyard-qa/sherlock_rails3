b328a0b54e6017a09d996150be19d2e6207ee190
Ian Morgan <ian@ruby-code.com>
2012-12-10 12:11:34 -0500

    Add Basic database testing

--- app/controllers/db_controller.rb
+++ app/controllers/db_controller.rb
@@ -2,4 +2,18 @@ class DbController < ApplicationController
   def process_list
     @results = ActiveRecord::Base.connection.execute('show processlist')
   end
+  
+  def all
+    
+    begin
+      @mongo = Mongo::MongoClient.new('localhost', 27017)
+    rescue
+      @mongo = false
+    end
+    
+    @memcached = Dalli::Client.new('localhost:11211')
+    @redis = Redis.new
+    @riak = Riak::Client.new
+    
+  end
 end

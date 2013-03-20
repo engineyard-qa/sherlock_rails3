bbd30e3e479f200e823cb4ebf14db49dff0e08fb
Dennis Bell <dennis.j.bell@gmail.com>
2013-03-19 17:25:46 -0700

    fixed error on detect schema

--- app/controllers/protocol_controller.rb
+++ app/controllers/protocol_controller.rb
@@ -2,7 +2,7 @@ class ProtocolController < ApplicationController
 
   
   def detect
-    schema = request.ssl? ? 'HTTP' : 'HTTPS'
+    @schema = request.ssl? ? 'HTTP' : 'HTTPS'
   end
 
   def force_ssl
